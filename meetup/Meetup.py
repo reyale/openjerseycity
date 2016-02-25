dc118ffecd91ee36a93c82cdfd0b09054e301dd1
alex <alex@alex-VirtualBox.(none)>
2016-02-24 23:32:40 -0500

    adding ability to limit results

--- meetup/Meetup.py
+++ meetup/Meetup.py
@@ -1,10 +1,15 @@
 import urllib2
 import json
 
-def get_events(meetup_url):
+def get_events(meetup_url, max_results=None):
     # example query
     # https://api.meetup.com/2/events?&sign=true&photo-host=public&group_urlname=Code-For-Jersey-City
 
     pattern = 'https://api.meetup.com/2/events?&sign=true&photo-host=public&group_urlname=%s' % meetup_url
+
+    if max_results is not None:
+        int(max_results) # cause it to bomb if it's not an int
+        pattern += '&page=' + str(max_results) 
+
     data = urllib2.urlopen(pattern).read()
     return json.loads(data)
