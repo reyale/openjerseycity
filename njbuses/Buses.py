89db27d6a5939bbbaf001db57e826c5b67dbacb2
Anthony Townsend <anthony@bitsandatoms.net>
2016-07-02 06:12:32 +0300

    added headsign, destination, and timestamp. untested

--- njbuses/Buses.py
+++ njbuses/Buses.py
@@ -5,13 +5,17 @@ _sources = {
 }
 
 class Bus:
-  def __init__(self, id_text, lat, lon):
+  def __init__(self, id_text, lat, lon, headsign, destination, timestamp):
      self.id_text = id_text
      self.lat = lat
      self.lon = lon
+     self.headsign = headsign
+     self.destination = destination
+     self.timestamp = timestamp
 
   def __repr__(self):
-     return 'bus[id=%s lat=%s long=%s]' % (self.id_text, self.lat, self.lon)
+     # need to format the time better?
+     return 'bus[id=%s lat=%s long=%s headsign=%s destination=%s time=%s]' % (self.id_text, self.lat, self.lon, self.headsign, self.destination, self.timestamp)
 
 def parse_xml_data(data):
     results = []
@@ -33,7 +37,19 @@ def parse_xml_data(data):
             continue
         lon = lons[0].text
 
-        results.append(Bus(id_text, lat, lon))
+    headsign = atype.findall('fs')
+    if len(fs) != 1:
+            continue
+        headsign = fs[0].text
+
+    destination = atype.findall('pd')
+    if len(pd) != 1:
+            continue
+        destination = pd[0].text
+
+    timestamp = mark
+
+    results.append(Bus(id_text, lat, lon))
     return results
 
 def get_data(source):
@@ -41,6 +57,10 @@ def get_data(source):
     if source not in _sources:
         raise AssertionError('Unknown source=%s valid sources=%s' % (key, str(sources.keys())))
 
+    # mark the time right before we fetch the source data
+    import datetime
+    mark = datetime.datetime.now()
+    
     import urllib2
     data = urllib2.urlopen(_sources[source]).read()
     return parse_xml_data(data)
