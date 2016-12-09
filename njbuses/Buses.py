4f24e8b81c7d1114f590a1ff6914f5cc1d96249c
alex <alex@lucca>
2016-12-08 20:47:35 -0500

    new bus grabber, grabs all the fields

--- njbuses/Buses.py
+++ njbuses/Buses.py
@@ -4,69 +4,40 @@ _sources = {
   'nj': 'http://mybusnow.njtransit.com/bustime/map/getBusesForRouteAll.jsp'
 }
 
-
 class Bus:
-    def __init__(self, id_text, lat, lon, headsign, destination, timestamp):
-        self.id_text = id_text
-        self.lat = lat
-        self.lon = lon
-        self.headsign = headsign
-        self.destination = destination
-        self.timestamp = timestamp
+  def __init__(self, **kwargs):
+     for k,v in kwargs.items():
+         setattr(self, k, v)
 
-    def __repr__(self):
-        return 'bus[id=%s lat=%s long=%s headsign=%s destination=%s time=%s]' % (self.id_text, self.lat, self.lon, self.headsign, self.destination, self.timestamp)
+  def __repr__(self):
+     line = []
+     for prop,value in vars(self).iteritems():
+       line.append((prop,value))
+     line.sort(key=lambda x : x[0])
+     out_string = ' '.join([k+'='+v for k,v in line]) 
+     return 'bus[%s]' % out_string
 
-
-def parse_xml_data(data, timestamp):
+def parse_xml_data(data):
     results = []
 
     e = xml.etree.ElementTree.fromstring(data)
-
     for atype in e.findall('bus'):
-        ids = atype.findall('id')
-        if len(ids) != 1:
-            continue
-        id_text = ids[0].text
+        fields = { }
+        for field in atype.getchildren():
+            if field.tag not in fields:
+                fields[field.tag] = field.text
 
-        lats = atype.findall('lat')
-        if len(lats) != 1:
-            continue
-        lat = lats[0].text
-
-        lons = atype.findall('lon')
-        if len(lons) != 1:
-            continue
-        lon = lons[0].text
-
-        headsigns = atype.findall('fs')
-        headsign = ''
-        if len(headsigns) > 0:
-            headsign = headsigns[0].text
-
-        destinations = atype.findall('pd')
-        destination = ''
-        if len(destinations) > 0:
-            destination = destinations[0].text
-
-        results.append(Bus(id_text, lat, lon, headsign, destination, timestamp))
-    
+        results.append(Bus(**fields))
     return results
-
 
 def get_data(source):
     source = source.lower()
     if source not in _sources:
-        raise AssertionError('Unknown source=%s valid sources=%s' % (source, str(_sources.keys())))
-    
+        raise AssertionError('Unknown source=%s valid sources=%s' % (key, str(sources.keys())))
+
     import urllib2
-    import datetime
- 
-    # mark the time right before we fetch the source data
-    mark = datetime.datetime.now()
     data = urllib2.urlopen(_sources[source]).read()
-     
-    return parse_xml_data(data, mark)
+    return parse_xml_data(data)
 
 def parse_xml_file(fname):
-    return parse_xml_file(open(fname, 'r').read())
+    return parse_xml_data(open(fname,'r').read())
